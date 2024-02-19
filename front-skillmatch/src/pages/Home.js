d8f6d38148689d885906326545ae2a3b4044aef9
Amine Salmane <amine.games2002@gmail.com>
2024-02-20 00:46:53 +0100

    added svg

--- front-skillmatch/src/pages/Home.js
+++ front-skillmatch/src/pages/Home.js
@@ -1,16 +1,32 @@
 import GlobalStyle from "../components/GlobalStyles";
 import Navbar from "../components/Navbar";
+import styled from "styled-components";
+import Add from "../images/add.svg"
+
 
 const Home = () => {
   return (
     <div>
       <GlobalStyle />
       <Navbar title="Resumer" />
-
+      <Center>
+        <StyledImg src={Add}/>
+      </Center>
     </div>
   );
 };
 
+const Center = styled.div `
+  position: absolute;
+  height: 500px;
+  transform: translate(-50%,-50%);
+  top: 50%;
+  left: 50%;
+`
+const StyledImg = styled.img`
+  max-height: 100%;
+  width: auto;
+`;
 
 
 
