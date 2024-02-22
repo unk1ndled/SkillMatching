f650a4c5885dc6dca409e67d397c519fa48cc914
Haithaaam21 <oumanhou.haitham0123@gmail.com>
2024-02-22 22:01:58 +0100

    Update KeywordController.java

--- back-skillmatch/SkillMatching/src/main/java/com/qalb/SkillMatching/Controllers/KeywordController.java
+++ back-skillmatch/SkillMatching/src/main/java/com/qalb/SkillMatching/Controllers/KeywordController.java
@@ -23,7 +23,7 @@ public class KeywordController {
         List<String> words = Arrays.asList(paragraph.split("[\\s,\\.,\\,]+")); // Split by whitespace, commas, or periods
 
         List<String> keywords = Arrays.asList(
-                "python","javascript","java","c++","c#","ruby","swift","kotlin","php","html","css","typescript","sql","go","scala","rust","dart","objective-c","perl","r","shell scripting","vue.js","angularjs","react","node.js","asp.net","express.js","flask","spring boot","ruby on rails","laravel","django","jquery","bootstrap","ember.js","backbone.js","meteor.js","tensorflow","pytorch","keras","caffe","scikit-learn","numpy","pandas","matplotlib","seaborn","unity","unreal engine","Kafka");
+                "python","javascript","java","c++","c#","ruby","swift","kotlin","php","html","css","typescript","sql","go","scala","rust","dart","objective-c","perl","r","shell scripting","vue.js","angularjs","react","node.js","asp.net","express.js","flask","spring boot","ruby on rails","laravel","django","jquery","bootstrap","ember.js","backbone.js","meteor.js","tensorflow","pytorch","keras","caffe","scikit-learn","numpy","pandas","matplotlib","seaborn","unity","unreal engine","kafka");
 
         // Filter words that are keywords
         Set<String> extractedKeywords = words.stream()
